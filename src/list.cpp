739a234bef2267db90fb6c79f9ac7836571257ae
tremr <tremr@mail.ru>
2018-06-07 15:05:12 +0300

    [initial] Base files added

---
+++ src/list.cpp
@@ -0,0 +1,35 @@
+// Copyright 2018 Roman Tremaskin. All Rights Reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "../include/list.h"
+
+
+namespace Tral
+{
+
+	List::List()
+	{}
+
+
+	List::~List()
+	{}
+
+	std::string List::get_string() const
+	{
+		return "<font style='color:#00A900;background-color:#FF0000'>Mar 26 06:52:59 <font style='color:#6495ED;background-color:#FFA500'>marked fragment</font> 2639.118 1677 filtered fragment</font>";
+	}
+
+} // namespace Tral
+
